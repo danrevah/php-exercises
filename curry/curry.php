6c95f5f5c12a94010ea884c3fbfd5d5886ebc91e
Dan Revah <danrevah89@gmail.com>
2015-06-20 20:34:02 +0300

    CUrry

--- curry/curry.php
+++ curry/curry.php
@@ -1,29 +1,5 @@
 <?php
 
-//function Curry(...$params)
-//{
-//    $func = &array_shift($params);
-//    $ref = new ReflectionFunction($func);
-//    $argNum = $ref->getParameters();
-//    $args = $params;
-//
-//    $innerCurry = call_user_func(function() use ($args, $func, $argNum) {
-//        $innerCurryHierarchy = function(...$innerParams) use (&$args, $func, $argNum, &$innerCurryHierarchy) {
-//            $args = array_merge($args, $innerParams);
-//
-//            print_r($args);
-//            return (count($args) >= count($argNum))
-//                ? $func(...$args)
-//                : $innerCurryHierarchy;
-//        };
-//        return $innerCurryHierarchy;
-//    });
-//
-//    return (count($ref->getParameters()) <= count($params))
-//        ? $func(...$params)
-//        :  $innerCurry;
-//}
-
 function Curry($func)
 {
     if ( ! $func instanceof Closure) {
