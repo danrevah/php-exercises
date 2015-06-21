0d636f6be28b15487e4b91971f9509ac008b4f7d
danr <danrevah89@gmail.com>
2015-06-21 12:45:34 +0300

    Basic calculator

---
+++ basic-calculator/test.php
@@ -0,0 +1,26 @@
+<?php
+require('index.php');
+
+class BasicCalculatorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testBasicCalculations()
+    {
+        // Can contain only '+', '-', '(', ')', and integers
+        $this->assertEquals(calc(' 1 + 1 '), 2);
+        $this->assertEquals(calc('9-5 + 2'), 6);
+
+    }
+
+    public function testMultiDigit()
+    {
+        // Accepting integers and not only digits
+        $this->assertEquals(calc('100 - 5'), 95);
+    }
+
+    public function testComplexCalculation()
+    {
+        // A bit more complex expressions
+        $this->assertEquals(calc('((5+3+(1+2)-9)+1) + (9+1)'), 13);
+    }
+}
